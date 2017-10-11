a25c806ee7423a29c1d2d20e0818b95482c34028
James Blocker <jamesblocker81@gmail.com>
2017-10-10 21:13:49 -0700

    add logic to generate number between 0 and 1000.

---
+++ getNumber.js
@@ -0,0 +1,8 @@
+function getNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+result = getNumber(0, 1001);
+console.log(result);
